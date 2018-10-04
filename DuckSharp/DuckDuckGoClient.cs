614e5927b02e550c901dc00da80f57fa7b23de0d
TheMulti0 <raphael@codeprecise.com>
2018-10-04 22:18:35 +0300

    CancellationToken support

--- DuckSharp/DuckDuckGoClient.cs
+++ DuckSharp/DuckDuckGoClient.cs
@@ -2,7 +2,7 @@
 using System.Collections.Specialized;
 using System.IO;
 using System.Net;
-using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Web;
 using System.Xml.Serialization;
@@ -14,35 +14,36 @@ namespace DuckSharp
 {
     public class DuckDuckGoClient
     {
-        private readonly string _query;
         private readonly ResponseFormat _format;
         private readonly string _applicationName;
-        private readonly bool _noHtml;
-        private readonly bool _skipDisambig;
-        private readonly bool _noRedirect;
+        private readonly bool _allowHtml;
+        private readonly bool _allowDisambig;
+        private readonly bool _allowRedirect;
         private readonly int _jsonPretty;
 
+        private CancellationToken _cancellationToken;
+
         public DuckDuckGoClient(
-            string query,
             ResponseFormat format = ResponseFormat.Json,
             string applicationName = "DuckSharp",
-            bool noHtml = false,
-            bool skipDisambig = false,
-            bool noRedirect = false,
-            int jsonPretty = 1)
+            bool allowHtml = true,
+            bool allowDisambig = true,
+            bool allowRedirect = true,
+            int jsonPretty = 1,
+            CancellationToken cancellationToken = default(CancellationToken))
         {
-            _query = query;
             _format = format;
             _applicationName = applicationName;
-            _noHtml = noHtml;
-            _skipDisambig = skipDisambig;
-            _noRedirect = noRedirect;
+            _allowHtml = allowHtml;
+            _allowDisambig = allowDisambig;
+            _allowRedirect = allowRedirect;
             _jsonPretty = jsonPretty;
+            _cancellationToken = cancellationToken;
         }
 
-        public async Task<InstantAnswer> Query()
+        public async Task<InstantAnswer> Query(string query)
         {
-            string url = BuildUrl();
+            string url = BuildUrl(query);
             string response = await GetResponse(url);
 
             return DeserializeObject(response);
@@ -67,7 +68,7 @@ namespace DuckSharp
         private async Task<string> GetResponse(string url)
         {
             Stream responseStream;
-            if (_noHtml)
+            if (_allowHtml)
             {
                 WebRequest request = WebRequest.CreateHttp(url);
                 WebResponse response = await request.GetResponseAsync();
@@ -79,20 +80,21 @@ namespace DuckSharp
                 var response = (HttpWebResponse) await request.GetResponseAsync();
                 responseStream = response.GetResponseStream();
             }
+            _cancellationToken.ThrowIfCancellationRequested();
 
             var reader = new StreamReader(responseStream ?? throw new InvalidOperationException());
             return await reader.ReadToEndAsync();
         }
 
-        private string BuildUrl()
+        private string BuildUrl(string query)
         {
             NameValueCollection queryBuilder = HttpUtility.ParseQueryString("https://api.duckduckgo.com/?");
-            queryBuilder["q"] = _query;
+            queryBuilder["q"] = query;
             queryBuilder["format"] = _format.ToString().ToLower();
             queryBuilder["t"] = _applicationName;
-            queryBuilder["no_redirect"] = (_noRedirect ? 1 : 0).ToString();
-            queryBuilder["no_html"] = (_noHtml ? 1 : 0).ToString();
-            queryBuilder["skip_disambig"] = (_skipDisambig ? 1 : 0).ToString();
+            queryBuilder["no_redirect"] = (!_allowRedirect ? 1 : 0).ToString();
+            queryBuilder["no_html"] = (!_allowHtml ? 1 : 0).ToString();
+            queryBuilder["skip_disambig"] = (!_allowDisambig ? 1 : 0).ToString();
             queryBuilder["pretty"] = _jsonPretty.ToString();
 
             return HttpUtility.UrlDecode(queryBuilder.ToString().Remove(0, 1));
